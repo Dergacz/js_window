4b6c524fb21b19972c71d52c8ec7e498fdf7e82d
Stanislau Dziarhach <sats96dergach@gmail.com>
2023-05-19 13:53:43 +0200

    fix timer

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,26 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  const modalState = {};
+  const modalState = {
+    form: null,
+    width: null,
+    height: null,
+    type: null,
+    profile: null
+  };
 
   changeModalState(modalState);
+  const deadline = '2023-05-20'
 
   modals();
   tabs('.glazing_slider','.glazing_block', '.glazing_content', 'active');
   tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
+  timer('.container1', deadline);
 });
---
+++ src/js/modules/timer.js
@@ -0,0 +1,56 @@
+const timer = (id, deadline) => {
+  const getTimeRemaining = (endTime) => {
+    const t = Date.parse(endTime) - Date.parse(new Date().toString());
+    const seconds = Math.floor((t / 1000) % 60);
+    const minutes = Math.floor((t / (1000 * 60)) % 60);
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+      'total': t,
+      'seconds': seconds,
+      'minutes': minutes,
+      'hours': hours,
+      'days': days
+    };
+  };
+
+  const setClock = (selector, endTime) => {
+    const timer = document.querySelector(selector);
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+    function updateClock () {
+      const t = getTimeRemaining(endTime);
+      days.textContent = addZero(t.days);
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if(t.total <= 0) {
+        days.textContent = '00';
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  const addZero = (num) => {
+    if(num <= 9) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  setClock(id, deadline)
+}
+
+export default timer;
